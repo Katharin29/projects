94a6933ca0c847201e56887e8dccfd36ceb6d0fe
Katharin29 <precildakatharin@gmail.com>
2024-10-14 20:37:00 +0530

    Add files via upload

---
+++ perfect number.java
@@ -0,0 +1,15 @@
+import java.util.*;
+public class Main{
+    public static void main(String[]args){
+        Scanner s=new Scanner(System.in);
+        int n=s.nextInt();
+        int sum=0;
+        int te=n;
+        for(int i=1;i<n;i++){
+            if(n%i==0){
+                sum=sum+i;
+            }
+        }
+        System.out.println((sum==te)?"1":"0");
+    }
+}
